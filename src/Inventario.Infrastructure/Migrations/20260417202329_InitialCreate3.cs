227b271cccf8aa67ca7ed17bc6d0a583ee1b7e8d
Freyser Ipanaque <freyser.ipanaque@dpworld.com>
2026-04-20 15:03:24 -0500

    update bd

---
+++ src/Inventario.Infrastructure/Migrations/20260417202329_InitialCreate3.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventario.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_active",
+                table: "sub_categories",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_active",
+                table: "locations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_active",
+                table: "categories",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_active",
+                table: "assets",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_active",
+                table: "asset_maintenances",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_active",
+                table: "asset_assignments",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_active",
+                table: "sub_categories");
+
+            migrationBuilder.DropColumn(
+                name: "is_active",
+                table: "locations");
+
+            migrationBuilder.DropColumn(
+                name: "is_active",
+                table: "categories");
+
+            migrationBuilder.DropColumn(
+                name: "is_active",
+                table: "assets");
+
+            migrationBuilder.DropColumn(
+                name: "is_active",
+                table: "asset_maintenances");
+
+            migrationBuilder.DropColumn(
+                name: "is_active",
+                table: "asset_assignments");
+        }
+    }
+}
